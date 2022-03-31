ea7f455760429c17bad74a1500115b848d44eb60
User <User@192.168.1.28>
2022-03-31 22:03:40 +0800

    lab3

---
+++ src/lights/RunningHolidayLights.java
@@ -0,0 +1,35 @@
+package lights;
+
+import java.util.List;
+
+public class RunningHolidayLights implements HolidayLights {
+	
+	int length = 0;
+	
+	/**
+	 * Creates new running holiday lights.
+	 * @param length - length of this set of lights.
+	 */
+	public RunningHolidayLights(int length) {
+		
+		length = this.length;
+		
+		// TODO
+		throw new RuntimeException("RunningHolidayLights(length) not yet implemented!");
+	}
+	
+	public List<Light> next() {
+		// TODO
+		throw new RuntimeException("RunningHolidayLights.next() not yet implemented!");
+	}
+	
+	/**
+	 * Returns the length of this
+	 * @return length of this
+	 */
+	public int getLength() {
+		// TODO
+		throw new RuntimeException("RunningHolidayLights.getLength() not yet implemented!");
+	}
+						
+}
